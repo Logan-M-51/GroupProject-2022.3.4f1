676421cd71d759f272a75ce97a2e40cbe9215dc9
Logan <loganmiller10825@gmail.com>
2023-07-29 16:33:37 -0700

    Got UI to update live with the value of the flag

--- My project/Assets/Scripts/varHandler.cs
+++ My project/Assets/Scripts/varHandler.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class varHandler : MonoBehaviour
 {
     public bool player_flag;
+    public TextMeshProUGUI flag_text;
     public int player_i;
     public int player_j;
     public int player_k;
@@ -19,11 +22,57 @@ public class varHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(player_flag);
+        //Debug.Log(player_flag);
     }
 
     public void toggleFlag()
     {
         player_flag = !player_flag;
+        toggleFlagUI();
+        
+    }
+
+    public void toggleFlagUI()
+    {   
+        if (player_flag)
+        {
+            flag_text.text = "true";
+        }
+        else
+        {
+            flag_text.text = "false";
+        }
+        Debug.Log(flag_text.text);
+        //flag_text.ForceMeshUpdate(true);
+    }
+
+    public void incrementI()
+    {
+        player_i++;
+    }
+
+    public void incrementJ()
+    {
+        player_j++;
+    }
+
+    public void incrementK()
+    {
+        player_k++;
+    }
+
+    public void decrementI()
+    {
+        player_i--;
+    }
+
+    public void decrementJ()
+    {
+        player_j--;
+    }
+
+    public void decrementK()
+    {
+        player_k--;
     }
 }

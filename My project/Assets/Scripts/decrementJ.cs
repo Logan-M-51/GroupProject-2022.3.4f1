dda356e3d75faea00a0b565812bcdf8b125e66e7
Logan <loganmiller10825@gmail.com>
2023-07-30 15:51:05 -0700

    ijk scripts and moved UI

---
+++ My project/Assets/Scripts/decrementJ.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class decrementJ : MonoBehaviour
+{
+    varHandler handler;
+
+    void Start()
+    {
+        handler = GameObject.Find("Player").GetComponent<varHandler>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("collision detected");
+        if (other.gameObject.name == "Sphere")
+        {
+            handler.decrementJ();
+        }
+    }
+}
---
+++ My project/Assets/Scripts/incrementJ.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class incrementJ : MonoBehaviour
+{
+    varHandler handler;
+
+    void Start()
+    {
+        handler = GameObject.Find("Player").GetComponent<varHandler>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("collision detected");
+        if (other.gameObject.name == "Sphere")
+        {
+            handler.incrementJ();
+        }
+    }
+}
+
